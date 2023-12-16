1150d51c527290b4427bfedad4d190b053532175
Roi Martin <jroi.martin@gmail.com>
2023-12-16 18:35:26 +0100

    poc-go-buildinfo: add command

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jroimartin/poc-go-buildinfo
+
+go 1.21.5
+
+require github.com/jroimartin/clilog v0.1.1
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"runtime/debug"
+
+	"github.com/jroimartin/clilog"
+)
+
+func main() {
+	h := clilog.NewCLIHandler(os.Stderr, &clilog.HandlerOptions{Level: slog.LevelInfo})
+	slog.SetDefault(slog.New(h))
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		fmt.Fprintln(os.Stderr, "not ok")
+		os.Exit(1)
+	}
+
+	fmt.Printf("%v %v\n", info.Main.Path, info.Main.Version)
+	for _, dep := range info.Deps {
+		fmt.Printf("%v %v\n", dep.Path, dep.Version)
+	}
+}
